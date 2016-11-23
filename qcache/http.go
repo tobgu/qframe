b58c60c4a7584eaba3fbd8f6c9343531c105b8c5
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2016-11-23 09:58:44 +0100

    Use development branch of gota to make use of new Reader and Writer compliant functions

--- qcache/http.go
+++ qcache/http.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"github.com/kniren/gota/data-frame"
-	"bytes"
 )
 
 
@@ -13,12 +12,8 @@ func NewDataset(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	contentType := r.Header.Get("Content-Type")
 	if contentType == "text/csv" {
-		// TODO: Get rid of this, ReadCSV should take a Reader
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-		s := buf.String()
-		frame := df.ReadCSV(s)
-		csvData, err := frame.SaveCSV()
+		frame := df.ReadCSV(r.Body)
+		err := frame.WriteCSV(w)
 
 		if err != nil {
 			http.Error(w, "Could not convert data to CSV", http.StatusInternalServerError)
@@ -26,11 +21,6 @@ func NewDataset(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.Header().Set("Content-Type", "text/csv")
-		_, err = w.Write(csvData)
-		if err != nil {
-			// Is there anything reasonable that could be done here?
-			log.Fatal(err)
-		}
 	}
 }
 
