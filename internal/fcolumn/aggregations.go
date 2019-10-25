febc26fa1c441b50d1898afa703d8b865d76fad9
xiaoyaoyouzizai <xiaoyaoyouzizai@gmail.com>
2019-10-25 18:39:12 -0400

    Update aggregations.go
    add avg

--- internal/fcolumn/aggregations.go
+++ internal/fcolumn/aggregations.go
@@ -2,6 +2,7 @@ package fcolumn
 
 var aggregations = map[string]func([]float64) float64{
 	"sum": sum,
+	"avg": avg,
 }
 
 func sum(values []float64) float64 {
@@ -10,4 +11,13 @@ func sum(values []float64) float64 {
 		result += v
 	}
 	return result
+}
+
+func avg (values []float64) float64 {
+	result := 0.0
+        for _, v := range values {
+		result += v
+	}
+
+	return result/float64(len(values))
 }
