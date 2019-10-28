a2670db43ec331eefebaaad0ca2dd4fc13cfa7fa
xiaoyaoyouzizai <xiaoyaoyouzizai@gmail.com>
2019-10-28 17:01:35 -0400

    gofmt

--- internal/fcolumn/aggregations.go
+++ internal/fcolumn/aggregations.go
@@ -1,23 +1,23 @@
 package fcolumn
 
 var aggregations = map[string]func([]float64) float64{
-	"sum": sum,
-	"avg": avg,
+        "sum": sum,
+        "avg": avg,
 }
 
 func sum(values []float64) float64 {
-	result := 0.0
-	for _, v := range values {
-		result += v
-	}
-	return result
+        result := 0.0
+        for _, v := range values {
+                result += v
+        }
+        return result
 }
 
-func avg (values []float64) float64 {
-	result := 0.0
+func avg(values []float64) float64 {
+        result := 0.0
         for _, v := range values {
-		result += v
-	}
+                result += v
+        }
 
-	return result/float64(len(values))
+        return result / float64(len(values))
 }
